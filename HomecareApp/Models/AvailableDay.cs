1cfae09ffe02f6ea2233dc3f08055c0255d23af3
peterandersenn <peand9319@oslomet.no>
2025-10-14 11:22:01 +0200

    validering

--- HomecareApp/Models/AvailableDay.cs
+++ HomecareApp/Models/AvailableDay.cs
@@ -1,11 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HomecareApp.Models;
 public class AvailableDay
 {
     public int AvailableDayId { get; set; }
+    
+    [Required(ErrorMessage = "Helsepersonell er påkrevd.")]
     public int HealthcarePersonnelId { get; set; }
+    
+    [Required(ErrorMessage = "Dato er påkrevd.")]
+    [DataType(DataType.Date)]
     public DateTime Date { get; set; }
+    
+    [Required(ErrorMessage = "Starttid er påkrevd.")]
+    [DataType(DataType.Time)]
     public TimeSpan StartTime { get; set; }
+
+    [Required(ErrorMessage = "Sluttid er påkrevd.")]
+    [DataType(DataType.Time)]
     public TimeSpan EndTime { get; set; }
+    
     public User? HealthcarePersonnel { get; set; }
     public List<Appointment>? Appointments { get; set; } 
 }
--- HomecareApp/Models/User.cs
+++ HomecareApp/Models/User.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HomecareApp.Models;
 
 public class User
 {
     public int UserId { get; set; }
+    
+    [Required(ErrorMessage = "Navn er påkrevd.")]
+    [StringLength(100, ErrorMessage = "Navnet kan ikke overstige 100 tegn.")]
     public string Name { get; set; }
+    
+    [Required(ErrorMessage = "E-post er påkrevd.")]
+    [EmailAddress(ErrorMessage = "Ugyldig e-postformat.")]
     public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Rolle er påkrevd.")]
     public string Role { get; set; } 
 }
