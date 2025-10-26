17a77a29a6fb3ca5ef879530a017893422abd6ca
Tahir Adil <tahir@kosli.com>
2025-10-27 00:09:02 +0100

    Fiksa sånn at man bare kan lage available day minimum en dag frem i tid

--- HomecareApp/Models/AvailableDay.cs
+++ HomecareApp/Models/AvailableDay.cs
@@ -1,16 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace HomecareApp.Models;
+
 public class AvailableDay
 {
     public int AvailableDayId { get; set; }
-    
+
     [Required(ErrorMessage = "Healthcare personell is required")]
-    public int HealthcarePersonnelId { get; set; } 
+    public int HealthcarePersonnelId { get; set; }
 
     [Required(ErrorMessage = "Date is required")]
     [DataType(DataType.Date)]
-    public DateTime Date { get; set; } 
+    [FutureDate(ErrorMessage = "Date has to be in future")]
+    public DateTime Date { get; set; }
 
     [Required(ErrorMessage = "Start time is required")]
     [DataType(DataType.Time)]
@@ -21,5 +23,15 @@ public class AvailableDay
     public TimeSpan EndTime { get; set; }
 
     public User HealthcarePersonnel { get; set; } = default!;
-    public List<Appointment>? Appointments { get; set; } 
+    public List<Appointment>? Appointments { get; set; }
+}
+
+
+// This is a custom validation attribute to make sure date is always in future (One day ahead)
+public class FutureDateAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        return value is DateTime date && date.Date > DateTime.Today;
+    }
 }
