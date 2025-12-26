1fc832ed90f0a551bcfac019d9f206e08bd5c537
peterandersenn <peand9319@oslomet.no>
2025-12-26 17:52:49 +0100

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/HomeCare-Project-25/',
+
   server: {
     // Porten React-applikasjonen kjører på (http://localhost:3000)
     port: 3000,
     // Proxy-innstilling: Sender alle kall som starter med /api til .NET-serveren
     proxy: {
       '/api': {
-        target: 'http://localhost:5011', 
+        target: 'http://localhost:5011',
         changeOrigin: true, // Endrer host-headeren til target-URLen
         secure: false,
       },
